4cff5e85305e99fc1fe498b7ff3874c00b03e9c3
Daniel Nikolovski <danneniko@gmail.com>
2015-06-01 12:00:28 +0200

    Lot of updates, blog etc

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 $(function() {
+	var toggles = document.querySelectorAll(".nav-switch");
 
-  var uparrow = $('#uparrow');
+	function addToggleHandler(toggle) {
+	    toggle.addEventListener("click", function(e) {
+	        e.preventDefault();
+	        (this.classList.contains("active") === true) ? this.classList.remove("active"): this.classList.add("active");
+	    });
+	}
 
-  uparrow.each(function(){
-      $(this).click(function(){ 
-          $('html,body').animate({ scrollTop: 0 }, 'normal');
-          return false; 
-      });
-  });
+	[].forEach.call(toggles, addToggleHandler);
 
+
+	$( ".nav-switch" ).click(function() {
+	    $("nav").toggleClass("nav-open");
+	    $("nav a").toggleClass("open-items");
+	});
 });
